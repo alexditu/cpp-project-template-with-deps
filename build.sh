23c751efbdc09a44f8af107a99d4271f437ff8ff
Alex Ditu <aditu@bitdefender.com>
2021-05-30 15:26:10 +0300

    initial setup

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BUILD_DIR="builddir"
+if [ $# -eq 1 ]; then
+	BUILD_DIR="$1"
+fi
+
+echo "Using build directory: ${BUILD_DIR}"
+
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}" || (echo "cd to ${BUILD_DIR} failed"; exit)
+
+cmake .. -DCMAKE_INSTALL_PREFIX=install
+cmake --build . --target install
